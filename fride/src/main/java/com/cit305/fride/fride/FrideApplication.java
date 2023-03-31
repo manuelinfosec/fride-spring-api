d625bef76a8750e77b4cb9cab066d6e5365f56b5
Manuel <manuelinfosec@gmail.com>
2023-03-31 01:52:47 +0100

    Create Maven Project: Fride

---
+++ fride/src/main/java/com/cit305/fride/fride/FrideApplication.java
@@ -0,0 +1,13 @@
+package com.cit305.fride.fride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrideApplication.class, args);
+	}
+
+}
---
+++ fride/src/test/java/com/cit305/fride/fride/FrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cit305.fride.fride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
